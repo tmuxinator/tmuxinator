57dc25310de27ca975f176d8e110dc88713561b5
Allen Bargi <allen.bargi@gmail.com>
2010-12-11 02:25:48 +0100

    added help and version actions and a bunch of aliases

--- lib/tmuxinator/assets/tmux_config.tmux
+++ lib/tmuxinator/assets/tmux_config.tmux
@@ -3,11 +3,11 @@ tmux start-server
 
 if ! $(tmux has-session -t <%= @project_name %>); then
 
-tmux new-session -d -s <%= @project_name %> -n <%= @tab[0].name %>
+tmux new-session -d -s <%= @project_name %> -n <%= @tabs[0].name %>
 tmux set-option -t <%= @project_name %> base-index 1
 
 <% @tabs.each do |tab| %>
-  <% unless @tab.index(tab) == 0 %>
+  <% unless @tabs.index(tab) == 0 %>
 tmux new-window -t <%= @project_name %>:<%= @tabs.index(tab) + 1 %> -n <%= tab.name %>
   <% end %>
 <% end %>
--- lib/tmuxinator/cli.rb
+++ lib/tmuxinator/cli.rb
@@ -2,10 +2,10 @@ require 'fileutils'
 
 module Tmuxinator
   class Cli
-    
+
     class << self
       include Tmuxinator::Helper
-      
+
       def start(*args)
         if args.empty?
           self.usage
@@ -18,9 +18,9 @@ module Tmuxinator
       def usage
         puts <<"DOC"
   Usage: tmuxinator ACTION [Arg]
-  
+
   ACTOINS:
-  open [project_name]                 
+  open [project_name]
       create a new project file and open it in your editor
   copy [source_project] [new_project]
       copy source_project project file to a new project called new_project
@@ -30,12 +30,18 @@ module Tmuxinator
       deletes all existing projects!
   list [-v]
       list all existing projects
+  help
+      shows this help document
+  version
+
 DOC
-        
       end
-      
+      alias :help :usage
+      alias :h :usage
+
       # Open a config file, it's created if it doesn't exist already.
       def open(*args)
+        exit!("You must specify a name for the new project") unless args.size > 0
         puts "warning: passing multiple arguments to open will be ignored" if args.size > 1
         @name = args.shift
         FileUtils.mkdir_p(root_dir+"scripts")
@@ -48,17 +54,18 @@ DOC
         system("$EDITOR #{config_path}")
         update_scripts
       end
-      
+      alias :o :open
+
       def copy(*args)
         @copy = args.shift
         @name = args.shift
         @config_to_copy = "#{root_dir}#{@copy}.yml"
-        
+
         exit!("Project #{@copy} doesn't exist!")             unless File.exists?(@config_to_copy)
         exit!("You must specify a name for the new project") unless @name
-        
+
         file_path = "#{root_dir}#{@name}.yml"
-        
+
         if File.exists?(file_path)
           confirm!("#{@name} already exists, would you like to overwrite it? (type yes or no):") do
             FileUtils.rm(file_path)
@@ -67,12 +74,13 @@ DOC
         end
         open(@name)
       end
-      
+      alias :c :copy
+
       def delete(*args)
         puts "warning: passing multiple arguments to delete will be ignored" if args.size > 1
         filename  = args.shift
         file_path = "#{root_dir}#{filename}.yml"
-        
+
         if File.exists?(file_path)
           confirm!("Are you sure you want to delete #{filename}? (type yes or no):") do
             FileUtils.rm(file_path)
@@ -81,9 +89,9 @@ DOC
         else
           exit! "That file doesn't exist."
         end
-        
       end
-      
+      alias :d :delete
+
       def implode(*args)
         exit!("delete_all doesn't accapt any arguments!") unless args.empty?
         confirm!("Are you sure you want to delete all tmuxinator configs? (type yes or no):") do
@@ -91,6 +99,7 @@ DOC
           puts "Deleted #{root_dir}"
         end
       end
+      alias :i :implode
 
       def list(*args)
         verbose = args.include?("-v")
@@ -101,38 +110,46 @@ DOC
           puts "    #{path}"
         end
       end
+      alias :l :list
+      alias :ls :list
+
+      def version
+        system("cat #{File.dirname(__FILE__) + '/../../VERSION'}") 
+        puts 
+      end
+      alias :v :version
       
       def update_scripts
         Dir["#{root_dir}*.tmux"].each {|p| FileUtils.rm(p) }
         aliases = []
-        Dir["#{root_dir}*.yml"].each do |path| 
+        Dir["#{root_dir}*.yml"].each do |path|
           path = File.basename(path, '.yml')
           aliases << Tmuxinator::ConfigWriter.new(path).write!
         end
         Tmuxinator::ConfigWriter.write_aliases(aliases)
       end
-      
-      def method_missing(method, *args, &block)  
-        puts "There's no command called #{method} in tmuxinator" 
-        usage 
+
+      def method_missing(method, *args, &block)
+        puts "There's no command called #{method} in tmuxinator"
+        usage
       end
 
       private #==============================
-            
+
       def root_dir
         "#{ENV["HOME"]}/.tmuxinator/"
       end
-      
+
       def sample_config
         "#{File.dirname(__FILE__)}/assets/sample.yml"
       end
-      
+
       def user_config
         @config_to_copy || "#{ENV["HOME"]}/.tmuxinator/default.yml"
       end
-      
+
     end
-    
+
   end
 end
 
