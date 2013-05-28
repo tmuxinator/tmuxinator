8f7758978a497cdf1cdaa061dce8155b26982f65
Christopher Chow <chris@chowie.net>
2013-05-28 21:49:49 +1000

    Add simplecov and coveralls.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require "coveralls"
+require "simplecov"
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 require "tmuxinator"
 
 SAMPLE_CONFIG = File.join(File.dirname(__FILE__), "..", "lib", "tmuxinator", "assets", "sample.yml")
--- tmuxinator.gemspec
+++ tmuxinator.gemspec
@@ -42,5 +42,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "simplecov"
+  spec.add_development_dependency "coveralls"
 end
 
