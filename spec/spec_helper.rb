a137463cda242185ea9c5bd9784f2eed6621dd15
Christopher Chow <chris@chowie.net>
2013-05-30 00:18:21 +1000

    Add a helper method for testing IO in RSpec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,19 @@ RBENV_SAMPLE_CONFIG = File.join(File.dirname(__FILE__), "..", "lib", "tmuxinator
 RSpec.configure do |config|
   config.order = "random"
 end
+
+# Copies from minitest.
+def capture_io
+  require 'stringio'
+
+  orig_stdout, orig_stderr = $stdout, $stderr
+  captured_stdout, captured_stderr = StringIO.new, StringIO.new
+  $stdout, $stderr = captured_stdout, captured_stderr
+
+  yield
+
+  return captured_stdout.string, captured_stderr.string
+ensure
+  $stdout = orig_stdout
+  $stderr = orig_stderr
+end
