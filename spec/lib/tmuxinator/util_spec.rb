03fb95473fec0cfa8b0cc680796ba8e636432699
Kevin Carter <cartkev@gmail.com>
2013-08-23 13:06:47 -0600

    Allow specifying an array of commands in the yaml

--- spec/lib/tmuxinator/util_spec.rb
+++ spec/lib/tmuxinator/util_spec.rb
@@ -1,4 +1,21 @@
 require "spec_helper"
 
 describe Tmuxinator::Util do
+  let(:util) { Object.new.extend(Tmuxinator::Util) }
+
+  describe "#flatten_command" do
+    it "flattens and joins an array" do
+      command = [
+        "command 1",
+        "",
+        "command 2",
+      ]
+      expect(util.flatten_command(command)).to eq("command 1 && command 2")
+    end
+
+    it "passes through a string" do
+      command = "command 1"
+      expect(util.flatten_command(command)).to eq("command 1")
+    end
+  end
 end
