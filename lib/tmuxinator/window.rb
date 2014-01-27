00fcdb28929df184480820ca2860a1c3db20ecb8
Magier Marcin <marcinmagier@gmail.com>
2014-01-27 16:03:13 +0100

    set window path if defined

--- lib/tmuxinator/window.rb
+++ lib/tmuxinator/window.rb
@@ -2,7 +2,7 @@ module Tmuxinator
   class Window
     include Tmuxinator::Util
 
-    attr_reader :name, :panes, :layout, :commands, :index, :project
+    attr_reader :name, :panes, :layout, :commands, :index, :project, :path
 
     def initialize(window_yaml, index, project)
       @name = window_yaml.keys.first.present? ? window_yaml.keys.first.shellescape : nil
@@ -11,6 +11,7 @@ module Tmuxinator
       @pre = nil
       @project = project
       @index = index
+      @path = project.root.shellescape
 
       value = window_yaml.values.first
 
@@ -18,6 +19,7 @@ module Tmuxinator
         @index = value["index"] if value["index"].present?
         @layout = value["layout"].present? ? value["layout"].shellescape : nil
         @pre = value["pre"] if value["pre"].present?
+        @path = value["path"] if value["path"].present?
 
         @panes = build_panes(value["panes"])
       else
@@ -78,7 +80,7 @@ module Tmuxinator
     end
 
     def tmux_new_window_command
-      "#{project.tmux} new-window -c #{project.root.shellescape} -t #{tmux_window_target} -n #{name}"
+      "#{project.tmux} new-window -c #{path} -t #{tmux_window_target} -n #{name}"
     end
 
     def tmux_layout_command
