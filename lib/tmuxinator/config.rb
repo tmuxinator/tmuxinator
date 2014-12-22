618c0a2ddb550441e27ebe89296317e60a4a1c4a
Evgeny Vereshchagin <evvers@ya.ru>
2014-12-22 17:35:53 +0000

    [Closes #274] Fix project path detection.

--- lib/tmuxinator/config.rb
+++ lib/tmuxinator/config.rb
@@ -44,7 +44,7 @@ module Tmuxinator
 
       def project(name)
         projects = Dir.glob("#{root}/**/*.yml")
-        project_file = projects.detect { |project| project =~ /^#{name}.yml$/ }
+        project_file = projects.detect { |project| File.basename(project, ".yml") == name }
         project_file || "#{root}/#{name}.yml"
       end
 
