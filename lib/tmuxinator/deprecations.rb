e5dc58c457421e40c5bd9f1b866be68d15a07f66
Mikica Ivosevic <mikica.ivosevic@gmail.com>
2015-09-17 11:14:00 +0200

    replace all methods with method_missing

--- lib/tmuxinator/deprecations.rb
+++ lib/tmuxinator/deprecations.rb
@@ -1,19 +1,11 @@
 module Tmuxinator
   module Deprecations
-    def rvm?
-      yaml["rvm"]
-    end
-
-    def rbenv?
-      yaml["rbenv"]
-    end
-
-    def pre_tab?
-      yaml["pre_tab"]
-    end
-
-    def cli_args?
-      yaml["cli_args"]
+    def method_missing(name, *args, &block)
+      {:rvm? => "rvm", :rbenv? => "rbenv", :pre_tab? => "pre_tab", :cli_args? => "cli_args"}.each do |key,value|
+        if key == name
+          return yaml[value]
+        end
+      end
     end
   end
 end
