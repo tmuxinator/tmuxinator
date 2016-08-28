12de4a1dc8e5b595c6d0bd1037298ac150ae9c23
Piotr Czarnecki <pioczarn@gmail.com>
2016-08-28 18:40:54 +0200

    Work around failing `Config.installed?` in spec

--- spec/lib/tmuxinator/cli_spec.rb
+++ spec/lib/tmuxinator/cli_spec.rb
@@ -280,6 +280,11 @@ describe Tmuxinator::Cli do
     # this command variant only works for tmux version 1.6 and up.
     context "from a session" do
       context "with tmux >= 1.6", if: Tmuxinator::Config.version >= 1.6 do
+        before do
+          # Necessary to make `Config.installed?` work in specs
+          allow(Tmuxinator::Config).to receive(:installed?).and_return(true)
+        end
+
         context "session exists" do
           before(:all) do
             # Can't add variables through `let` in `before :all`.
