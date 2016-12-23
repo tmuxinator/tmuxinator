858a86e029b81fa311717f88f20fc88066db1ccb
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-12-23 16:44:03 +0100

    extract hook common functionality

--- lib/tmuxinator/hooks.rb
+++ lib/tmuxinator/hooks.rb
@@ -1,4 +1,13 @@
 module Tmuxinator
   module Hooks
+    module_function
+
+    def commands_from(hook_config)
+      if hook_config.is_a?(Array)
+        hook_config.join("; ")
+      else
+        hook_config
+      end
+    end
   end
 end
--- lib/tmuxinator/hooks/project.rb
+++ lib/tmuxinator/hooks/project.rb
@@ -2,41 +2,21 @@ module Tmuxinator
   module Hooks
     module Project
       module_function
-      
+
       def hook_on_reattach
-        reattach_config = yaml["on_reattach"]
-        if reattach_config.is_a?(Array)
-          reattach_config.join("; ")
-        else
-          reattach_config
-        end
+        Tmuxinator::Hooks.commands_from yaml["on_reattach"]
       end
 
       def hook_on_detach
-        detach_config = yaml["on_detach"]
-        if detach_config.is_a?(Array)
-          detach_config.join("; ")
-        else
-          detach_config
-        end
+        Tmuxinator::Hooks.commands_from yaml["on_detach"]
       end
 
       def hook_on_start
-        start_config = yaml["on_start"]
-        if start_config.is_a?(Array)
-          start_config.join("; ")
-        else
-          start_config
-        end
+        Tmuxinator::Hooks.commands_from yaml["on_start"]
       end
 
       def hook_on_stop
-        stop_config = yaml["on_stop"]
-        if stop_config.is_a?(Array)
-          stop_config.join("; ")
-        else
-          stop_config
-        end
+        Tmuxinator::Hooks.commands_from yaml["on_stop"]
       end
     end
   end
